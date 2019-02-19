53c3dcb553de56358dfde3af8b87fb08169089fa
luiscmartinez <mrluismartinezzz@gmail.com>
2019-02-19 06:01:37 -0800

    finsihed contact outline structure

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,34 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <h3>Drop us a line.</h3>
-      <p>
-        Contact us anytime with general<br />questions, comments, or concerns,
-        by<br /> filling out the form below. You may also <br />reach us by
-        phone at: 626-286-3055
-      </p>
+      <div className='content_container'>
+        <div className='left'>
+          <h3>Drop us a line.</h3>
+          <p>
+            Contact us anytime with general<br />questions, comments, or
+            concerns, by<br /> filling out the form below. You may also <br />reach
+            us by phone at: 626-286-3055
+          </p>
+          <h3>WE DO CATERING</h3>
+          <p>
+            <strong>Pepe's Mexican Food</strong>
+            <br />5829 Rosemead Blvd.<br />Temple City, CA 91780
+          </p>
+        </div>
+        <h1 className='right'>HERE WILL GO THE MAP</h1>
+      </div>
+      <div className='content_container'>
+        <img
+          className='lol'
+          src='http://theoriginalpepes.com/wp-content/uploads/2016/07/DSC9697.jpg'
+        />
+        <form>
+          <input type='text' placeholder='name' />
+          <input type='email' placeholder='Email' />
+          <textarea placeholder='Message' />
+          <button type='submit'>CONTACT US</button>
+        </form>
+      </div>
     </div>
   )
 }
