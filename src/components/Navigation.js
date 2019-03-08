23a8ce957fc46f790edfaecb247aef3e1870bf7d
luiscmartinez <mrluismartinezzz@gmail.com>
2019-03-07 17:05:44 -0800

    added hamburger

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,11 @@ import Home from './Home'
 const Navigation = () => {
   return (
     <div className='nav'>
+      <svg className='svg' width='27' height='24'>
+        <line x1='3' y1='3' x2='24' y2='3' />
+        <line x1='3' y1='10' x2='24' y2='10' />
+        <line x1='3' y1='16' x2='24' y2='16' />
+      </svg>
       <div className='inner_nav'>
         <h2>
           <Link to='/'>home</Link>
