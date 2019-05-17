0c5f72fb8cea43c0b04773c49d0bd650eac78b79
luis <mrluismartinezzz@gmail.com>
2019-05-17 12:35:50 -0700

    removed FB footer img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,9 +15,6 @@ const Footer = () => {
         />
       </Link>
       <Navigation />
-      <div>
-        <img src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTIgMGMtNi42MjcgMC0xMiA1LjM3My0xMiAxMnM1LjM3MyAxMiAxMiAxMiAxMi01LjM3MyAxMi0xMi01LjM3My0xMi0xMi0xMnptMyA4aC0xLjM1Yy0uNTM4IDAtLjY1LjIyMS0uNjUuNzc4djEuMjIyaDJsLS4yMDkgMmgtMS43OTF2N2gtM3YtN2gtMnYtMmgydi0yLjMwOGMwLTEuNzY5LjkzMS0yLjY5MiAzLjAyOS0yLjY5MmgxLjk3MXYzeiIvPjwvc3ZnPg==' />
-      </div>
       <footer>
         <p>© 2019 The Original Pepe's</p>
       </footer>
